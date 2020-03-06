5c9cef06f51c6ce0255c4ad7387fd25bc6b02fbf
Eduardo Pereira do Carmo <ecarmo@outlok.com>
2020-03-06 14:31:14 -0800

    finished contactForm

--- src/components/common/contactForm.js
+++ src/components/common/contactForm.js
@@ -1,8 +1,12 @@
 // React
 import React, { Component } from 'react';
 
+// Npm package
+import InputMask from 'react-input-mask';
+
 // Custom Components
-import Button from './button';
+import Loading from '../common/loading';
+import ErrorFeedback from '../common/errorFeedback';
 
 // Image
 import heroImage from '../../assets/img/codingkids_logo.png'; // with import
@@ -11,51 +15,254 @@ import heroImage from '../../assets/img/codingkids_logo.png'; // with import
 import './contactForm.css';
 
 class ContactForm extends Component{
-    saveForm(){
-        alert('saveForm');
+
+    constructor(props){
+        super(props);
+
+        // Form Default Value
+        this.state = {
+            dayOfTheClass: {
+                errors: null,
+                isLoaded: false,
+                items: []
+            }, 
+            disableSubmitBtn: false
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    // Load data from API
+    componentDidMount(){
+        // Load data
+        fetch('http://api.codingkids.wmdd.ca/class', {
+            method: 'get',
+            headers: new Headers({
+                'content-type': 'application/json'
+            })
+        }).then(response => {
+            if(response.status !== 200){
+                this.setState({
+                    dayOfTheClass: {
+                        isLoaded: true
+                    }
+                });
+                throw response.statusText;
+            }else{
+                return response.json();
+            }
+        })
+        .then(result => {
+            if(result != null 
+                && result.status != null 
+                && result.status.id === 200){
+                    // Map result
+                    let items = [];
+                    if(Array.isArray(result.data) && result.data.length > 0){
+                        for(let i = 0; i < result.data.length; i++){
+                            let name = result.data[i].name;
+                            if(Array.isArray(result.data[i].sessions) && result.data[i].sessions.length > 0){
+                                for(let z = 0; z < result.data[i].sessions.length; z++){
+                                    items.push({
+                                        value: result.data[i].sessions[z].id,
+                                        text: `${name} (${(new Date(result.data[i].sessions[z].start)).toLocaleDateString("en-US")})`
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    // Set State
+                    this.setState({
+                        dayOfTheClass: {
+                            isLoaded: true,
+                            items: items
+                        }
+                    });
+            }else {
+                this.setState({
+                    dayOfTheClass: {
+                        isLoaded: true,
+                        errors: result.status.errors
+                    }
+                });
+            }
+        }).catch(error => {
+            this.setState({
+                dayOfTheClass: {
+                    isLoaded: true,
+                    errors: [ error ]
+                }
+            });
+        });
+    }
+
+    // Handle all Input Changing in the Form
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    // Handle the form submition of the form
+    handleSubmit(event) {
+        // Prevent Default behavior
+        event.preventDefault();
+
+        // Get state values
+        let {
+                childAge, 
+                childName, 
+                dayOfTheClass, 
+                email, 
+                note, 
+                parentName, 
+                parentPhoneNumber, 
+                session} = this.state;
+
+
+        // Check values
+        if(session === undefined){
+            session = dayOfTheClass.items[0].value
+        }
+
+        // Disable the form
+        this.setState({
+            disableSubmitBtn: true
+        });
+
+        // Submit the to the API
+        let valueToSubit = {
+            "sessionId": session,
+            "email": email,
+            "parentName": parentName,
+            "parentPhone": parentPhoneNumber,
+            "childName": childName,
+            "childAge": childAge,
+            "note": note === null ? "" : note
+        }
+
+        // Submit data
+        fetch('http://api.codingkids.wmdd.ca/booking/addNew', {
+            method: 'post',
+            headers: new Headers({
+                'content-type': 'application/json'
+            }),
+            body: JSON.stringify(valueToSubit)
+        }).then(response => {
+            if(response.status !== 200){
+                this.setState({
+                    disableSubmitBtn: false
+                });
+                throw response.statusText;
+            }else{
+                return response.json();
+            }
+        })
+        .then(result => {
+            if(result != null 
+                && result.status != null 
+                && result.status.id === 200){
+                    alert('Submit success');
+                    // Set State
+                    this.setState({
+                        childAge: null, 
+                        childName: null, 
+                        dayOfTheClass, 
+                        email, 
+                        note, 
+                        parentName, 
+                        parentPhoneNumber, 
+                        session,
+                        disableSubmitBtn: false
+                    });
+            }else {
+                console.log(result.status.errors)
+                alert('Submit fail');
+                this.setState({
+                    disableSubmitBtn: false
+                });
+            }
+        }).catch(error => {
+            console.log(error)
+            alert('Submit fail');
+            this.setState({
+                disableSubmitBtn: false
+            });
+        });
+    }
+
+    // Select Day of the Class
+    renderDayOfTheClass(){
+        const {isLoaded, errors, items} = this.state.dayOfTheClass;
+        const disableSubmitBtn = this.state.disableSubmitBtn;
+        
+        // Is Loading that from API
+        if(!isLoaded){
+            return <Loading />
+        }else if(Array.isArray(errors) && errors.length > 0){
+            // Has error
+            return <ErrorFeedback errorsToShow={errors} />
+        }else if(Array.isArray(items) && items.length > 0){
+            // Show the Days of Classes
+            return (
+                <select name="session" onChange={this.handleInputChange} disabled={disableSubmitBtn} required>
+                    {
+                        items.map(function(session, index){
+                            return (
+                                <option value={session.value} key={index}>{session.text}</option>
+                            ) 
+                        })
+                    }
+                </select>
+            )
+        }else{
+            return ("");
+        }
+    }
+
+    // Render the Component
     render(){
+        const disableSubmitBtn = this.state.disableSubmitBtn;
         return (
             <section className="contactForm">
                 <h1>Book a trial class</h1>
                 <img src={heroImage} alt="Coding Kids" />
-                <form className="form__horizontal">
+                <form className="form__horizontal" onSubmit={this.handleSubmit}>
                     <div className="form__horizontal_line">
                         <label>Day of the class</label>
-                        <select>
-                            <option value="1">Day 1</option>
-                            <option value="2">Day 2</option>
-                            <option value="3">Day 3</option>
-                            <option value="4">Day 4</option>
-                        </select>
+                        {this.renderDayOfTheClass()}
                     </div>
                     <div className="form__horizontal_line">
                         <label>E-mail</label>
-                        <input type="text"/>
+                        <input type="email" name="email" onChange={this.handleInputChange} disabled={disableSubmitBtn} required/>
                     </div>
                     <div className="form__horizontal_line">
                         <label>Parent's / Caregiver's Name </label>
-                        <input type="text"/>
+                        <input type="text" name="parentName" onChange={this.handleInputChange} disabled={disableSubmitBtn} required/>
                     </div>
                     <div className="form__horizontal_line">
                         <label>Parent's / Caregiver's Phone Number</label>
-                        <input type="text"/>
+                        <InputMask mask="(999) 999 9999" maskChar=" " name="parentPhoneNumber" onChange={this.handleInputChange} disabled={disableSubmitBtn} required />
                     </div>
                     <div className="form__horizontal_line">
                         <label>Child's (Children's) Name</label>
-                        <input type="text"/>
+                        <input type="text" name="childName" onChange={this.handleInputChange} disabled={disableSubmitBtn} required/>
                     </div>
                     <div className="form__horizontal_line">
                         <label>Child's (Children's) Age / Grade</label>
-                        <input type="text"/>
+                        <input type="number" size="6" min="4" max="12" name="childAge" onChange={this.handleInputChange} disabled={disableSubmitBtn} required/>
                     </div>
                     <div className="form__horizontal_line">
-                        <Button 
-                            classList="primary" 
-                            action={(e) => this.saveForm()}
-                            dataKey={Date.now()}
-                            text="Apply" />
+                        <label>Note</label>
+                        <textarea rows="4" name="note" onChange={this.handleInputChange} disabled={disableSubmitBtn} />
+                    </div>
+                    <div className="form__horizontal_line">
+                        <button type="submit" className="primary" disabled={disableSubmitBtn}>Send</button>
                     </div>
                 </form>
             </section>
